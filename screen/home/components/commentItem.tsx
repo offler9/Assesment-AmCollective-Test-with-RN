0d239e4f3a401cc705a57d3bdebae6dfdc165b3b
offler <offler@rushowl.sg>
2024-05-18 20:55:06 +0700

    update style

--- screen/home/components/commentItem.tsx
+++ screen/home/components/commentItem.tsx
@@ -22,25 +22,27 @@ const CommentItem = ({
   
   const FlatListCommentReplyItem = ({item } :{item: ArticleCommentRepliesProps}) => (
     <View style={{ paddingHorizontal: 8 }}>
-      <View style={{ flexDirection: 'row', alignItems: 'center', gap: 5 }}>
-        <Text style={{ fontSize: 14.5, fontWeight: '500' }}>{item?.username}</Text>
-        <Text style={{ fontSize: 11 }}>{moment(item?.timestamp).fromNow()}</Text>
-      </View>
-      <Text style={{ fontSize: 13.5, paddingLeft: 2}}>
-        <Text style={{fontSize: 13, textDecorationLine: 'underline', fontWeight: '500'}}>
-          {`@${item?.username} `}
+      <View style={{ marginBottom: 6}}>
+        <View style={{ flexDirection: 'row', alignItems: 'center', gap: 5 }}>
+          <Text style={{ fontSize: 14.5, fontWeight: '500' }}>{item?.username}</Text>
+          <Text style={{ fontSize: 11 }}>{moment(item?.timestamp).fromNow()}</Text>
+        </View>
+        <Text style={{ fontSize: 13.5, paddingLeft: 2}}>
+          <Text style={{fontSize: 13, textDecorationLine: 'underline', fontWeight: '500'}}>
+            {`@${item?.username} `}
+          </Text>
+          {item?.reply_text}
         </Text>
-        {item?.reply_text}
-      </Text>
-      <TouchableOpacity onPress={() => handlePressReply('2-reply', item)}>
-        <Text style={{ fontSize: 13, fontWeight: '500', opacity: 0.7}}>Reply</Text>
-      </TouchableOpacity>
+        <TouchableOpacity onPress={() => handlePressReply('2-reply', item)}>
+          <Text style={{ fontSize: 13, fontWeight: '500', opacity: 0.7}}>Reply</Text>
+        </TouchableOpacity>
+      </View>
       
       {/* render reply to reply comment */}
         {item?.replies?.length > 0 && (
-          <View style={{ paddingVertical: 6, paddingHorizontal: 10}}>
+          <View style={{ marginHorizontal: 6, paddingHorizontal: 7, borderLeftWidth: 0.3, paddingVertical: 4}}>
             {item?.replies?.map((repliesItem) => (
-              <>
+              <View style={{ marginBottom: 6}}>
                 <View key={repliesItem?.reply_id} style={{ flexDirection: 'row', alignItems: 'center', gap: 5}}>
                   <Text style={{ fontSize: 14, fontWeight: '500' }}>{repliesItem?.username}</Text>
                   <Text style={{ fontSize: 11 }}>{moment(repliesItem?.timestamp).fromNow()}</Text>
@@ -51,7 +53,7 @@ const CommentItem = ({
                  </Text>
                   {repliesItem.reply_text}
                 </Text>
-              </>
+              </View>
             ))}
             </View>
           )}
@@ -65,12 +67,12 @@ const CommentItem = ({
           <Text style={{ fontSize: 15.5, fontWeight: '600'}}>{username}</Text>
           <Text style={{ fontSize: 11}}>{moment(timestamp).fromNow()}</Text>
         </View>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingVertical: 5 }}>
-        <Text style={{ fontSize: 14.5, paddingLeft: 2}}>{commentText}</Text>
-        {/* <TouchableOpacity onPress={handleDeleteComment}>
-          <EvilIcons name="trash" size={18} color="#d13126" />
-        </TouchableOpacity> */}
-      </View>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingVertical: 5 }}>
+          <Text style={{ fontSize: 14.5, paddingLeft: 2}}>{commentText}</Text>
+          {/* <TouchableOpacity onPress={handleDeleteComment}>
+            <EvilIcons name="trash" size={18} color="#d13126" />
+          </TouchableOpacity> */}
+        </View>
       </View>
       <TouchableOpacity onPress={() => handlePressReply('1-reply')}>
         <Text style={{ fontSize: 12.5, fontWeight: '500', opacity: 0.7}}>Reply</Text>
@@ -81,7 +83,9 @@ const CommentItem = ({
           onPress={() => setShowRepliesComment(!showRepliesCommentData)}
           style={{ paddingHorizontal: 4}}
         >
-          <Text style={{ fontSize: 13, color: '#858585', paddingHorizontal: 10, paddingVertical: 8 }}>{showRepliesCommentData ? 'hide replies' : `show more replies(${repliesTotal})`}</Text>
+          <Text style={{ fontSize: 13, color: '#858585', paddingHorizontal: 10, paddingVertical: 7 }}>
+            {showRepliesCommentData ? 'hide replies' : `show more replies(${repliesTotal})`}
+          </Text>
         </TouchableOpacity>
       )}
 
